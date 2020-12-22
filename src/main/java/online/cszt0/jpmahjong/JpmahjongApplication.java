966f43deef8e78830737859a044025ae2c989479
chushi0 <wangyongchao49@sina.com>
2020-12-22 19:04:05 +0800

    initialize project

---
+++ src/main/java/online/cszt0/jpmahjong/JpmahjongApplication.java
@@ -0,0 +1,13 @@
+package online.cszt0.jpmahjong;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpmahjongApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpmahjongApplication.class, args);
+	}
+
+}
---
+++ src/test/java/online/cszt0/jpmahjong/JpmahjongApplicationTests.java
@@ -0,0 +1,13 @@
+package online.cszt0.jpmahjong;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpmahjongApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
